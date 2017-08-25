12a3d49370d9733d61946e0f366495fa6304aac9
asribeiro.rafael <asribeiro.rafael@gmail.com>
2017-08-25 03:15:56 -0300

    - Front-End: Em Testes - Removido do Projeto; - Back-End: Em Funcionamento;

--- back_end/modules/deliveries/deliveries.route.js
+++ back_end/modules/deliveries/deliveries.route.js
@@ -2,6 +2,6 @@ const DeliveriesController = require('./deliveries.controller.js');
 
 module.exports = (app) => {
     app.post('/deliveries/add', DeliveriesController.add);
-    app.get('/deliveries/remove/:deliveriesId', DeliveriesController.remove);
+    app.get('/deliveries/remove', DeliveriesController.remove);
     app.get('/deliveries/getall', DeliveriesController.getAll);
 };
